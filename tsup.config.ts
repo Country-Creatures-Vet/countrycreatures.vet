f22abcc6e32332e5ff7a0117db68011028db69cf
Wulf0123 <bmr0123@hotmail.com>
2025-02-28 20:38:28 -0800

    this deploys. but doesn't work. running into 404 and home not being what i expect it to be.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     format: ["cjs", "esm"],
-    entry: ["./src/index.ts"],
+    entry: ["./src/main.tsx"],
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
     clean: true,
     sourcemap: true, // TODO make this false for prod
+    noExternal: [".*"], // Forces all dependencies to be bundled
 });
